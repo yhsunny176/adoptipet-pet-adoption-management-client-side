2cf759da304284d60b5f52bfa684e54c62692dab
yhsunny176 <yeasinulhaq763@gmail.com>
2025-07-11 02:08:38 +0600

    Removed async keyword and added setLoading in authData in AuthProvide.jsx

--- src/layouts/Authentication/AuthProvider.jsx
+++ src/layouts/Authentication/AuthProvider.jsx
@@ -22,38 +22,38 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const createUser = async (email, password) => {
+    const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
-    const updateUser = async (updatedData) => {
+    const updateUser = (updatedData) => {
         setLoading(true);
         return updateProfile(auth.currentUser, updatedData);
     };
 
-    const signInUser = async (email, password) => {
+    const signInUser = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
 
-    const signInWithGoogle = async () => {
+    const signInWithGoogle = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider);
     };
 
-    const signInWithFacebook = async () => {
+    const signInWithFacebook = () => {
         setLoading(true);
         return signInWithPopup(auth, facebookProvider);
     };
 
-    const logOut = async () => {
+    const logOut = () => {
         setLoading(true);
         return signOut(auth);
     };
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
             setLoading(false);
         });
